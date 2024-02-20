70d7f1136e927221f714c2e46f00de0e821c720c
ostrrovska <ostrovskakatia@gmail.com>
2024-02-20 15:37:57 +0200

    Add Apartment class

---
+++ Apartment.h
@@ -0,0 +1,15 @@
+
+#ifndef RESTAURANTBOOKING_APARTMENT_H
+#define RESTAURANTBOOKING_APARTMENT_H
+#include <string>
+
+class Apartment {
+private:
+    std::string typeOfApartment;
+    int apartmentNumber;
+    int numberOfRooms;
+    int price;
+};
+
+
+#endif //RESTAURANTBOOKING_APARTMENT_H
