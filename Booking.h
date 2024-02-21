a90433458b62615ee6e51a29cca2e23dfc3def0e
ostrrovska <ostrovskakatia@gmail.com>
2024-02-21 18:20:01 +0200

    Add Booking class

---
+++ Booking.h
@@ -0,0 +1,17 @@
+#ifndef RESTAURANTBOOKING_BOOKING_H
+#define RESTAURANTBOOKING_BOOKING_H
+#include <string>
+#include "Customer.h"
+
+class Booking {
+private:
+    Customer customer;
+    std::string dateOfArrival;
+    std::string dateOfDeparture;
+public:
+
+
+};
+
+
+#endif //RESTAURANTBOOKING_BOOKING_H
