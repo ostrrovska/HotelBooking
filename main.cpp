65707a5cd14964188024ce7bc2aca9d60e131c95
ostrrovska <ostrovskakatia@gmail.com>
2024-02-22 17:33:36 +0200

    Add Customer class objects and vector <customers>

--- main.cpp
+++ main.cpp
@@ -11,4 +11,10 @@ int main() {
     Apartment apartment2("Lux",502,6,12000);
     Apartment apartment3("Basic",121,2,2500);
 
+    vector<Customer> customers;
+    Customer customer1("Katia",100456,18,"+380984563433");
+    Customer customer2("Maria",205677,17,"+380675554342");
+    Customer customer3("Nazar",690087,18,"+380985674462");
+
+
 }
