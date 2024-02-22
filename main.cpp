a8714cfffaa7738ba8fd25bec314f1048469511d
ostrrovska <ostrovskakatia@gmail.com>
2024-02-22 17:23:54 +0200

    Add Apartment class objects

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Apartment.h"
+#include "Customer.h"
+#include "Booking.h"
 using namespace std;
 
+
 int main() {
+
+    Apartment apartment1("Comfort", 201, 3, 5000);
+    Apartment apartment2("Lux",502,6,12000);
+    Apartment apartment3("Basic",121,2,2500);
 
 }
