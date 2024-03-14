eebed2e870bea7dfeb5ccfc2f8a7edc37b659deb
ostrrovska <ostrovskakatia@gmail.com>
2024-03-14 20:53:00 +0200

    Add Description class as interface with pure virtual function displayInfo

---
+++ Description.h
@@ -0,0 +1,12 @@
+
+#ifndef RESTAURANTBOOKING_DESCRIPTION_H
+#define RESTAURANTBOOKING_DESCRIPTION_H
+
+
+class Description {
+    public:
+    virtual void displayInfo() const = 0;
+};
+
+
+#endif //RESTAURANTBOOKING_DESCRIPTION_H
